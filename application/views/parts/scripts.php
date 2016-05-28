7880b77a48ae9948ea1378477c7afb5137754972
Byron Peña <byronpenna@gmail.com>
2016-05-28 14:13:31 -0600

    iniciales

---
+++ application/views/parts/scripts.php
@@ -0,0 +1,6 @@
+<!-- terceros -->
+	<script type="text/javascript" src=<?php echo base_url("content/js/terceros/jquery/jquery-2.2.4.min.js") ?> ></script>
+	<script type="text/javascript" src=<?php echo base_url("content/css/terceros/bootstrap/js/bootstrap.js")?> ></script>
+<!-- personalizados -->
+	<script type="text/javascript" src=<?php echo base_url("content/js/universales/functions.js") ?>></script>
+	<script type="text/javascript" src=<?php echo base_url("content/js/universales/script.js") ?>></script>
---
+++ application/views/url/welcome/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Inicio</title>
+	<?php
+		$this->load->view("parts/head.php");
+	?>
+	<link rel="stylesheet" type="text/css" href=<?php echo base_url("content/css/paginas/welcome/style.css") ?> >
+	<link rel="stylesheet" type="text/css" href=<?php echo base_url("content/css/paginas/welcome/media.css") ?> >
+</head>
+<body>
+	<div></div>
+	<?php
+		$this->load->view("parts/scripts.php");
+	?>
+
+</body>
+
+</html>
